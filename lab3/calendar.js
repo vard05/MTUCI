20d86d633870d0356a3b5840980d35973b3a6c63
vard05 <a.a.arkhipov05@gmail.com>
2023-12-28 15:37:43 +0300

    добавил 5 и 6 лабы

--- lab3/calendar.js
+++ lab3/calendar.js
@@ -13,5 +13,7 @@ var n = d.getDay();
 var month = d.getMonth()
 var day = d.getDate()
 
+month += 1
+
 document.getElementById('dow').innerHTML = days[n]
 document.getElementById('fd').innerHTML = `${day}.${('0' + month).slice(-2)}`
